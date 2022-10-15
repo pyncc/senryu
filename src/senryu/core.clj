805b87026fe0068e8f3f4dca01e1ff60d42977c0
Colin Lappala <colin@amperity.com>
2022-10-14 17:30:03 -0700

    getting some word/stream processing ideas out there

--- src/senryu/core.clj
+++ src/senryu/core.clj
@@ -1,5 +1,7 @@
 (ns senryu.core
-  "A not-very-serious haiku generator")
+  "A not-very-serious haiku generator"
+  (:import
+    java.util.Scanner))
 
 
 ;; Steps
@@ -24,8 +26,8 @@
 (def syllable-boundary-match (re-pattern "[^aeuioAEUIO][aeuioAEUIO]"))
 
 
-(defn word->syllable
-  "Return an integer count of a string's English language syllable count."
+(defn word->syllables
+  "Return an integer of an English word's (approximate) syllable count."
   [word]
   (if (< (count word) 5)
     1
@@ -34,3 +36,48 @@
          (count) ; count the syllable boundary matches
          (inc) ; include the first sound in the word in the count
          )))
+
+
+;; upon adding new word, review existing word-counts
+
+
+(defn update-state-from-word
+  "Update collector with counts, possibly including boundaries of various
+  syllable aggregations."
+  [collector word]
+  (let [syllables (word->syllables word)]
+       ;; (swap! collector update :counts conj (word->syllables word))
+       ;; tracking: syllable collections of 5 and 7
+       ;; determine overlapping syllable sequences as index or range coordinates
+       ;; {:fives #{[0 1 2 3]}
+       ;;  :sevens #{[1 2 3 4 5]}}
+       ;; to give the best chance of finding a solution
+       ))
+
+
+;; interesting to think about:
+;; ? can this, would this ever emit more than one stanza at a time?
+;; : since the evaluation is happening each word, probably not since
+;;   previous steps would have 'greedily' emited a solution previously
+;;   if possible
+(defn emit-stanza
+  "Scan words, boundaries and emit stanza(s?)"
+  [collector]
+  ;; find non-overlapping sequences from syllable counts
+  ;; and try to put stanzas together
+  ;; once we have two 5s and one 7, emit the stanza
+  ;; and clear these from the tracking state
+  )
+
+
+(defn process
+  [input]
+  (let [collector (atom {:counts []})]
+    (->> (iterator-seq (Scanner. input))
+         ;; compute word-at-a-time from word stream
+         ;; emit stanza-at-a-time
+         (map (partial update-state-from-word collector))
+
+         ;; this runs each word, alternatively this could be decoupled somehow...
+         ;; we also want to make sure there is a flush step, where emit-stanza
+         (keep (fn [_] (emit-stanza collector))))))
